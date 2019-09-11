0be175a9d7d9c05c74df09dea4c2a6367d8c89bb
pankrawat844 <pankrawat9@gmail.com>
2019-09-12 00:53:16 +0530

    Firast

---
+++ app/src/main/java/com/app/medicalapp/activity/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.app.medicalapp.activity.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.medicalapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
