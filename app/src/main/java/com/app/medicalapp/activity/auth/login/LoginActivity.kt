8c9dc56555b8678b8df33437374e823026e8a59a
pankdev09 <pankawat9@gmail.com>
2019-09-12 17:50:18 +0530

    Loin n Singup page

--- app/src/main/java/com/app/medicalapp/activity/auth/login/LoginActivity.kt
+++ app/src/main/java/com/app/medicalapp/activity/auth/login/LoginActivity.kt
@@ -1,4 +1,4 @@
-package com.app.medicalapp.activity.auth
+package com.app.medicalapp.activity.auth.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
---
+++ app/src/main/java/com/app/medicalapp/activity/auth/signup/Doctor_Signup.kt
@@ -0,0 +1,13 @@
+package com.app.medicalapp.activity.auth.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.medicalapp.R
+
+class Doctor_Signup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctor_signup)
+    }
+}
