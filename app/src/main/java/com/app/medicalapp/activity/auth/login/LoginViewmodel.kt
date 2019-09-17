2bf48aa172cf66dd4dba00ba445606d690ccdef6
Sumit <sumitjangir554@gmail.com>
2019-09-17 17:44:06 +0530

    modified

--- app/src/main/java/com/app/medicalapp/activity/auth/login/LoginViewmodel.kt
+++ app/src/main/java/com/app/medicalapp/activity/auth/login/LoginViewmodel.kt
@@ -2,7 +2,6 @@ package com.app.medicalapp.activity.auth.login
 
 import android.content.Intent
 import android.view.View
-import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import com.app.medicalapp.activity.auth.AuthListner
 import com.app.medicalapp.activity.auth.signup.SignUpActivity
